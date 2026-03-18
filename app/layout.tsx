0b0e3c75fb1fdcabb961191d3f60f64db7c65004
h-ichi <h.ichimiya.dosapo@gmail.com>
2026-03-18 17:27:37 +0900

    geogle analitics導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "わたしのレオパ BLOG",
@@ -29,6 +30,19 @@ export default function RootLayout({
         </main>
 
         <Footer />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KP0W9TTBGX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KP0W9TTBGX');
+          `}
+        </Script>
 
       </body>
     </html>
